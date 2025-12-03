1cb18f4ff72dba23abe404b41a4f0bb8c625ce92
protestor6 <p7777-3333@qq.com>
2025-12-03 21:43:47 +0800

    做完了图的大体结构、文件读写和显示

--- yys/graph.cpp
+++ yys/graph.cpp
@@ -26,10 +26,12 @@ void fileInit(Graph &G){
 		printf("请依次输入每个地点的名称（仅限英文字母，最大长度为20，不能有空格）\n");
 		for(int i=0;i<G.vexnum;i++)
 			scanf("%s",G.vexs[i]);
-		printf("输入邻接矩阵（-1表示两点不通，其他数字代表有航班连通两地，数字为票务表中对应航班）\n");
-		for(int i=0;i<G.vexnum;i++)
+		printf("输入邻接矩阵（0表示两点不通，其他数字代表有航班连通两地，数字为票务表中对应航班）\n");
+		for(int i=0;i<G.vexnum;i++){
+			printf("第%d/%d行：",i+1,G.vexnum);
 			for(int j=0;j<G.vexnum;j++)
 				scanf("%d",&G.arcs[i][j]);
+		}
 		//开始录入信息
 		FILE *fout;
 		fout=fopen("info.txt","w");
@@ -50,13 +52,14 @@ void fileInit(Graph &G){
 	for(int i=0;i<G.vexnum;i++)
 		for(int j=0;j<G.vexnum;j++)
 			fscanf(fin,"%d",&G.arcs[i][j]);
+	fclose(fin); 
 }
 void showGraph(const Graph& G){
 	printf("地点数：%d 航班数：%d\n",G.vexnum,G.arcnum);
 	printf("图中每个地点名称：\n");
 	for(int i=0;i<G.vexnum;i++)
 		printf("%s\n",G.vexs[i]);
-	printf("邻接矩阵（-1表示两点不通，其他数字代表有航班连通两地，数字为票务表中对应航班）：\n"); 
+	printf("邻接矩阵（0表示两点不通，其他数字代表有航班连通两地，数字为票务表中对应航班）：\n"); 
 	for(int i=0;i<G.vexnum;i++){
 		for(int j=0;j<G.vexnum;j++)
 			printf("%d ",G.arcs[i][j]);
